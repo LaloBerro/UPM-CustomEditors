73bd9e10172ea474925d450e9594128b9248d7bc
Lalo <laurencioberro@hotmail.com>
2022-04-13 14:52:54 -0300

    [A] Add Namespace to AssemblyReloadEditor

--- Editor/Custom Windows/AssemblyReloadEditor.cs
+++ Editor/Custom Windows/AssemblyReloadEditor.cs
@@ -1,23 +1,45 @@
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-public class AssemblyReloadEditor : EditorWindow
+namespace UtilitiesCustomPackage.EditorExtensions
 {
-    [MenuItem("Custom Editor/Assembly Reload Editor")]
-    public static void Init()
+    public class AssemblyReloadEditor : EditorWindow
     {
-        GetWindow<AssemblyReloadEditor>();
-    }
+        [MenuItem("Custom Editor/Assembly Reload Editor")]
+        public static void Init()
+        {
+            GetWindow<AssemblyReloadEditor>();
+        }
 
-    private void OnGUI()
-    {
-        if (GUILayout.Button(EditorGUIUtility.IconContent("IN LockButton on act@2x")))
+        private void OnGUI()
+        {
+            if (GUILayout.Button(EditorGUIUtility.IconContent("IN LockButton on act@2x")))
+            {
+                Debug.Log("Lock Assemblies");
+                EditorApplication.LockReloadAssemblies();
+            }
+
+            if (GUILayout.Button(EditorGUIUtility.IconContent("IN LockButton act@2x")))
+            {
+                Debug.Log("Unlock Assemblies");
+                EditorApplication.UnlockReloadAssemblies();
+
+                AssetDatabase.Refresh();
+            }
+
+            Repaint();
+        }
+
+        [MenuItem("Assets/Lock Reload Assemblies")]
+        public static void LockReloadAssemblies()
         {
             Debug.Log("Lock Assemblies");
             EditorApplication.LockReloadAssemblies();
         }
 
-        if (GUILayout.Button(EditorGUIUtility.IconContent("IN LockButton act@2x")))
+        [MenuItem("Assets/Unlock Reload Assemblies")]
+        public static void UnlockReloadAssemblies()
         {
             Debug.Log("Unlock Assemblies");
             EditorApplication.UnlockReloadAssemblies();
@@ -25,22 +47,41 @@ public class AssemblyReloadEditor : EditorWindow
             AssetDatabase.Refresh();
         }
 
-        Repaint();
+
     }
 
-    [MenuItem("Assets/Lock Reload Assemblies")]
-    public static void LockReloadAssemblies()
+    public class CreateMVPFolderStructure
     {
-        Debug.Log("Lock Assemblies");
-        EditorApplication.LockReloadAssemblies();
-    }
+        [MenuItem("Assets/Create MVP Folders")]
+        public static void Create()
+        {
+            string directoryPath = AssetDatabase.GetAssetPath(Selection.activeObject);
 
-    [MenuItem("Assets/Unlock Reload Assemblies")]
-    public static void UnlockReloadAssemblies()
-    {
-        Debug.Log("Unlock Assemblies");
-        EditorApplication.UnlockReloadAssemblies();
+            if (!Directory.Exists(directoryPath))
+            {
+                directoryPath = Path.GetDirectoryName(directoryPath);
+            }
 
-        AssetDatabase.Refresh();
+            Debug.Log(directoryPath);
+
+            CreateDirectory("Model", directoryPath);
+            CreateDirectory("View", directoryPath);
+            CreateDirectory("Presenter", directoryPath);
+        }
+
+        private static void CreateDirectory(string folderName, string existingPath)
+        {
+            string folderPath = Path.Combine(existingPath, folderName);
+
+            if (Directory.Exists(folderPath))
+            {
+                Debug.Log("The path is already created: " + folderPath);
+                return;
+            }
+
+            Directory.CreateDirectory(folderPath);
+
+            AssetDatabase.Refresh();
+        }
     }
 }
