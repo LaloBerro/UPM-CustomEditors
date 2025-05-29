e9f7fc9ac3a1a95a21c6c568a29109d9a4fb4733
Lalo <laurencioberro@hotmail.com>
2025-05-29 16:28:57 -0300

    [U] Improved ScreenshotTaker

--- Editor/Custom Windows/ScreenshotWindow.cs
+++ Editor/Custom Windows/ScreenshotWindow.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace UtilitiesCustomPackage.EditorExtensions.Windows
@@ -6,170 +8,197 @@ namespace UtilitiesCustomPackage.EditorExtensions.Windows
     [ExecuteInEditMode]
     public class ScreenshotWindow : EditorWindow
     {
-        private int resWidth = Screen.width * 4;
-        private int resHeight = Screen.height * 4;
+        private int _resolutionWidth = Screen.width * 4;
+        private int _resolutionHeight = Screen.height * 4;
+        private int _scale = 1;
 
-        private Camera camera;
-        private int scale = 1;
+        private Camera _camera;
+        private string _savePath = string.Empty;
+        private string _lastScreenshotPath = string.Empty;
 
-        private string path = "";
-        private string lastScreenshot = "";
-
-        private bool isTakeHiResShot = false;
+        private bool _hasToIncludeCanvas = true;
+        private bool _mustTakeHighResShot = false;
 
         [MenuItem("Tools/Screenshot")]
-        private static void ShowWindow()
+        private static void OpenWindow()
         {
-            EditorWindow editorWindow = EditorWindow.GetWindow(typeof(ScreenshotWindow));
-
-            editorWindow.autoRepaintOnSceneChange = true;
-            editorWindow.Show();
-            editorWindow.titleContent = new GUIContent("Screenshot");
+            ScreenshotWindow window = GetWindow<ScreenshotWindow>();
+            window.autoRepaintOnSceneChange = true;
+            window.titleContent = new GUIContent("Screenshot");
+            window.Show();
         }
 
         private void OnGUI()
         {
+            DrawResolutionSettings();
+            DrawCanvasToggle();
+            DrawSavePathField();
+            DrawCameraSelection();
+            DrawDefaultOptions();
+            DrawScreenshotButton();
+            DrawBottomActions();
+
+            if (_mustTakeHighResShot)
+                CaptureScreenshot();
+        }
+
+        private void DrawResolutionSettings()
+        {
             EditorGUILayout.LabelField("Resolution", EditorStyles.boldLabel);
-            resWidth = EditorGUILayout.IntField("Width", resWidth);
-            resHeight = EditorGUILayout.IntField("Height", resHeight);
-
+            _resolutionWidth = EditorGUILayout.IntField("Width", _resolutionWidth);
+            _resolutionHeight = EditorGUILayout.IntField("Height", _resolutionHeight);
+            _scale = EditorGUILayout.IntSlider("Scale", _scale, 1, 15);
             EditorGUILayout.Space();
+        }
 
-            scale = EditorGUILayout.IntSlider("Scale", scale, 1, 15);
-
-            EditorGUILayout.HelpBox("The default mode of screenshot is crop - so choose a proper width and height. The scale is a factor " +
-                "to multiply or enlarge the renders without loosing quality.", MessageType.None);
-
+        private void DrawCanvasToggle()
+        {
+            _hasToIncludeCanvas = EditorGUILayout.Toggle("Include Canvas", _hasToIncludeCanvas);
             EditorGUILayout.Space();
+        }
 
-            GUILayout.Label("Save Path", EditorStyles.boldLabel);
-
+        private void DrawSavePathField()
+        {
+            EditorGUILayout.LabelField("Save Path", EditorStyles.boldLabel);
             EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.TextField(path, GUILayout.ExpandWidth(false));
+
+            EditorGUILayout.TextField(_savePath, GUILayout.ExpandWidth(false));
             if (GUILayout.Button("Browse", GUILayout.ExpandWidth(false)))
-                path = EditorUtility.SaveFolderPanel("Path to Save Images", path, Application.dataPath);
+                _savePath = EditorUtility.SaveFolderPanel("Path to Save Images", _savePath, Application.dataPath);
 
             EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.HelpBox("Choose the folder in which to save the screenshots ", MessageType.None);
             EditorGUILayout.Space();
+        }
 
-            GUILayout.Label("Select Camera", EditorStyles.boldLabel);
-
-            camera = EditorGUILayout.ObjectField(camera, typeof(Camera), true, null) as Camera;
-
-            if (camera == null)
-                camera = Camera.main;
-
-            EditorGUILayout.HelpBox("Choose the camera of which to capture the render. You can make the background transparent using the transparency option.", MessageType.None);
-
+        private void DrawCameraSelection()
+        {
+            EditorGUILayout.LabelField("Select Camera", EditorStyles.boldLabel);
+            _camera = EditorGUILayout.ObjectField(_camera, typeof(Camera), true, null) as Camera;
+            if (_camera == null) 
+                _camera = Camera.main;
+            
             EditorGUILayout.Space();
-            EditorGUILayout.BeginVertical();
+        }
+
+        private void DrawDefaultOptions()
+        {
             EditorGUILayout.LabelField("Default Options", EditorStyles.boldLabel);
-
 
             if (GUILayout.Button("Set To Screen Size"))
             {
-                resHeight = (int)Handles.GetMainGameViewSize().y;
-                resWidth = (int)Handles.GetMainGameViewSize().x;
+                Vector2 size = Handles.GetMainGameViewSize();
+                _resolutionWidth = (int)size.x;
+                _resolutionHeight = (int)size.y;
             }
 
             if (GUILayout.Button("Default Size"))
             {
-                resHeight = 1440;
-                resWidth = 2560;
-                scale = 1;
+                _resolutionWidth = 1920;
+                _resolutionHeight = 1080;
+                _scale = 1;
             }
 
-            EditorGUILayout.EndVertical();
-
             EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Screenshot will be taken at " + resWidth * scale + " x " + resHeight * scale + " px", EditorStyles.boldLabel);
+        }
+
+        private void DrawScreenshotButton()
+        {
+            EditorGUILayout.LabelField($"Screenshot will be taken at {_resolutionWidth * _scale} x {_resolutionHeight * _scale} px", EditorStyles.boldLabel);
 
             if (GUILayout.Button("Take Screenshot", GUILayout.MinHeight(60)))
             {
-                if (path == "")
-                {
-                    path = EditorUtility.SaveFolderPanel("Path to Save Images", path, Application.dataPath);
-                    Debug.Log("Path Set");
-                    TakeHiResShot();
-                }
-                else
-                {
-                    TakeHiResShot();
-                }
+                if (string.IsNullOrEmpty(_savePath))
+                    _savePath = EditorUtility.SaveFolderPanel("Path to Save Images", _savePath, Application.dataPath);
+
+                StartScreenshotCapture();
             }
 
             EditorGUILayout.Space();
+        }
+
+        private void DrawBottomActions()
+        {
             EditorGUILayout.BeginHorizontal();
 
             if (GUILayout.Button("Open Last Screenshot", GUILayout.MaxWidth(160), GUILayout.MinHeight(40)))
+                OpenLastScreenshot();
+
+            if (GUILayout.Button("Open Folder", GUILayout.MaxWidth(100), GUILayout.MinHeight(40)))
+                OpenScreenshotFolder();
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void CaptureScreenshot()
+        {
+            if (!_camera)
+                throw new InvalidOperationException("Camera is null. Please assign a camera.");
+
+            Canvas[] canvases = FindObjectsByType<Canvas>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID);
+
+            if (_hasToIncludeCanvas)
             {
-                if (lastScreenshot != "")
+                foreach (Canvas canvas in canvases)
                 {
-                    Application.OpenURL("file://" + lastScreenshot);
-                    Debug.Log("Opening File " + lastScreenshot);
+                    canvas.renderMode = RenderMode.ScreenSpaceCamera;
+                    canvas.worldCamera = _camera;
                 }
             }
 
-            if (GUILayout.Button("Open Folder", GUILayout.MaxWidth(100), GUILayout.MinHeight(40)))
-            {
+            int width = _resolutionWidth * _scale;
+            int height = _resolutionHeight * _scale;
 
-                Application.OpenURL("file://" + path);
+            RenderTexture renderTexture = new RenderTexture(width, height, 24);
+            _camera.targetTexture = renderTexture;
+            RenderTexture.active = renderTexture;
+
+            _camera.Render();
+
+            Texture2D screenshot = new Texture2D(width, height, TextureFormat.RGB24, false);
+            screenshot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+            screenshot.Apply();
+
+            _camera.targetTexture = null;
+            RenderTexture.active = null;
+            DestroyImmediate(renderTexture);
+
+            string filePath = GetScreenshotPath(width, height);
+            File.WriteAllBytes(filePath, screenshot.EncodeToPNG());
+
+            Debug.Log($"✅ Screenshot saved to: {filePath}");
+            Application.OpenURL(filePath);
+
+            if (_hasToIncludeCanvas)
+            {
+                foreach (Canvas canvas in canvases)
+                    canvas.renderMode = RenderMode.ScreenSpaceOverlay;
             }
 
-            if (GUILayout.Button("More Assets", GUILayout.MaxWidth(100), GUILayout.MinHeight(40)))
-            {
-                Application.OpenURL("https://www.assetstore.unity3d.com/en/#!/publisher/5951");
-            }
-
-            EditorGUILayout.EndHorizontal();
-
-
-            if (isTakeHiResShot)
-            {
-                int resWidthN = resWidth * scale;
-                int resHeightN = resHeight * scale;
-                RenderTexture rt = new RenderTexture(resWidthN, resHeightN, 24);
-                camera.targetTexture = rt;
-
-                TextureFormat tFormat = TextureFormat.RGB24;
-
-                Texture2D screenShot = new Texture2D(resWidthN, resHeightN, tFormat, false);
-                camera.Render();
-                RenderTexture.active = rt;
-                screenShot.ReadPixels(new Rect(0, 0, resWidthN, resHeightN), 0, 0);
-                camera.targetTexture = null;
-                RenderTexture.active = null;
-                byte[] bytes = screenShot.EncodeToPNG();
-                string filename = ScreenShotName(resWidthN, resHeightN);
-
-                System.IO.File.WriteAllBytes(filename, bytes);
-                Debug.Log(string.Format("Took screenshot to: {0}", filename));
-                Application.OpenURL(filename);
-                isTakeHiResShot = false;
-            }
-
-            EditorGUILayout.HelpBox("In case of any error, make sure you have Unity Pro as the plugin requires Unity Pro to work.", MessageType.Info);
+            _mustTakeHighResShot = false;
         }
 
-        private string ScreenShotName(int width, int height)
+        private string GetScreenshotPath(int width, int height)
         {
-            string strPath = "";
-
-            strPath = string.Format("{0}/screen_{1}x{2}_{3}.png",
-                                 path,
-                                 width, height,
-                                           System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
-            lastScreenshot = strPath;
-
-            return strPath;
+            _lastScreenshotPath = Path.Combine(_savePath, $"screen_{width}x{height}_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png");
+            return _lastScreenshotPath;
         }
 
-        private void TakeHiResShot()
+        private void StartScreenshotCapture()
         {
-            Debug.Log("Taking Screenshot");
-            isTakeHiResShot = true;
+            Debug.Log("📸 Taking Screenshot...");
+            _mustTakeHighResShot = true;
+        }
+
+        private void OpenLastScreenshot()
+        {
+            if (!string.IsNullOrEmpty(_lastScreenshotPath))
+                Application.OpenURL("file://" + _lastScreenshotPath);
+        }
+
+        private void OpenScreenshotFolder()
+        {
+            if (!string.IsNullOrEmpty(_savePath))
+                Application.OpenURL("file://" + _savePath);
         }
     }
 }
